d7811245a4dd2471e6e52a7966d05c6ff17a6194
estercouto <estercouto@gmail.com>
2024-02-28 16:34:04 -0300

    first commit

---
+++ componets/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './componets/header/header'
+import Main from './componets/main/main'
+import Footer from './componets/footer/footer'
+
+export default function App() {
+  return (
+    <>
+      <Header/>
+      <Main/>
+      <Footer/>
+    </>
+  )
+}
+
+
